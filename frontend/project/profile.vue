a7234a6c66948c35248ef34622e833cce57ea344
riffca <riffca@yandex.ru>
2016-09-24 23:32:22 +0300

    make profile component logic

--- frontend/project/profile.vue
+++ frontend/project/profile.vue
@@ -1,18 +1,49 @@
 <template>
+
+<div id="profile">
+	<h1>Отзывы {{ user.name }}</h1>	
+	<div class="messages">
+		<message v-for="message in messages" :message="message"></message>
+	</div>
+</div>
+
 </template>
 
 <script>
+import message from '../base/message';
 export default {
-
-  name: 'component_name',
-
+  components: {
+  	message
+  },
   data () {
     return {
-
+    	isSelfPage: false
     };
+  },
+  ready(){
+  	this.$http.get('get-user/'+this.$route.params.user_id)
+  	.then((res)=>{
+  		this.user = res.data;
+
+  	})
+  	.then(()=>{
+  	   this.$http.get('get-user-messages/'+this.$route.params.user_id)
+  	   .then(res=>{
+  	   		this.messages = res.data; 
+  	   })
+  	});
   }
 };
 </script>
 
-<style lang="css" scoped>
+<style lang="sass">
+#profile {
+	h1 {
+		text-align: center;
+	}
+
+
+}
+
+
 </style>
